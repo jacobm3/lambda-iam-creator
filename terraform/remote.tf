75f77d3733928a8fce99b8e95a48b11549a76d12
Jacob Martinson <jacobm3@gmail.com>
2022-07-15 15:02:24 -0500

    updates.

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jm3-state"
-    key    = "NAME.tfstate"
+    key    = "lambda-iam-creator.tfstate"
     region = "us-east-1"
     #dynamodb_table  = "jm3-state"
   }
